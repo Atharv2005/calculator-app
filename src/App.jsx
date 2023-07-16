e549166d2a6053eb4d20874ef694defde37ebeef
atharv2005 <atharvdamle2005@gmail.com>
2023-07-16 19:39:43 +0530

    assigning values to the states

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,14 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 
 function App() {
-  const [displayOperads, setDisplayOperands] = useState(0)
+  const [displayOperads, setDisplayOperands] = useState("")
   const [operand1, setOperand1] = useState(null)
   const [operand2, setOperand2] = useState(null)
   const [operator, setOperator] = useState(null)
 
   const handleButtonClick = (value) => {
-    console.log(value)
     setDisplayOperands((previosValue) => previosValue + value)
-    console.log(typeof (value))
+    // console.log(typeof (value))
     // Clear value
 
     if (value === "C") {
@@ -19,19 +18,26 @@ function App() {
       setOperand2(null)
       return
     }
-
-    if (value === "Backspace") {
-      setDisplayOperands((previosValue) => previosValue - value)
-
-    }
-
     // setting operators
     if (['+', '-', '*', '/'].includes(value)) {
       setOperator(value)
-      console.log(operator)
+    }
+
+    if (operator === null && displayOperads !== null) {
+      setOperand1(parseFloat(displayOperads))
+    }
+
+    if (operand1 !== null && operator !== null) {
+      setOperand2(displayOperads)
     }
 
   }
+
+  useEffect(() => {
+    console.log("this is operand 1 " + operand1)
+    console.log("this is operand2 " + operand2)
+  }, [operand1, operand2])
+
   return (
     <div>
       <div>
@@ -39,21 +45,21 @@ function App() {
       </div>
 
       <div>
-        <button value="7" onClick={() => handleButtonClick(+"7")}> 7 </button >
-        <button value="8" onClick={() => handleButtonClick(+"8")}> 8</button >
-        <button value="9" onClick={() => handleButtonClick(+"9")}> 9</button >
+        <button value="7" onClick={() => handleButtonClick("7")}> 7 </button >
+        <button value="8" onClick={() => handleButtonClick("8")}> 8</button >
+        <button value="9" onClick={() => handleButtonClick("9")}> 9</button >
       </div>
 
       <div>
-        <button value="4" onClick={() => handleButtonClick(+"4")}> 4</button >
-        <button value="5" onClick={() => handleButtonClick(+"5")}> 5</button >
-        <button value="6" onClick={() => handleButtonClick(+"6")}> 6</button >
+        <button value="4" onClick={() => handleButtonClick("4")}> 4</button >
+        <button value="5" onClick={() => handleButtonClick("5")}> 5</button >
+        <button value="6" onClick={() => handleButtonClick("6")}> 6</button >
       </div>
 
       <div>
-        <button value="1" onClick={() => handleButtonClick(+"1")} > 1</button >
-        <button value="2" onClick={() => handleButtonClick(+"2")}> 2</button >
-        <button value="3" onClick={() => handleButtonClick(+"3")}> 3</button >
+        <button value="1" onClick={() => handleButtonClick("1")} > 1</button >
+        <button value="2" onClick={() => handleButtonClick("2")}> 2</button >
+        <button value="3" onClick={() => handleButtonClick("3")}> 3</button >
       </div>
 
       <div>
